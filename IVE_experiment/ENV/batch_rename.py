c49ac00962a98464e7e71d8fc9c7c37a4c86031a
payam tabrizian <ptabriz@ncsu.edu>
2017-03-07 21:18:14 -0500

    Added_the_IVE_folder

---
+++ IVE_experiment/ENV/batch_rename.py
@@ -0,0 +1,34 @@
+﻿#rename#
+import os
+
+IMAGE_PATH = os.path.join(os.getcwd())
+#FILE_PATH = os.getcwd()
+#fileList = os.getcwd()
+fileList = os.listdir(IMAGE_PATH)
+
+for im_name in fileList:
+ if im_name.endswith('_cubefront.png'):
+   orig=im_name
+   replace = im_name[:-13] + 'posz.png'
+   os.rename(orig,replace)
+ if im_name.endswith('_cubeback.png'):
+   orig=im_name
+   replace = im_name[:-12] + 'negz.png'
+   os.rename(orig,replace)
+ if im_name.endswith('_cubetop.png'):
+   orig=im_name
+   replace = im_name[:-11] + 'posy.png'
+   os.rename(orig,replace)
+ if im_name.endswith('_cubebottom.png'):
+   orig=im_name
+   replace = im_name[:-14] + 'negy.png'
+   os.rename(orig,replace)
+ if im_name.endswith('_cubeleft.png'):
+   orig=im_name
+   replace = im_name[:-12] + 'negx.png'
+   os.rename(orig,replace)
+ if im_name.endswith('_cuberight.png'):
+   orig=im_name
+   replace = im_name[:-13] + 'posx.png'
+   os.rename(orig,replace)
+ 
